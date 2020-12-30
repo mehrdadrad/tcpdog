1833b38f2805ae1302d1ff6e3aefc577ba626a07
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-12-29 21:07:45 -0800

    add more tests

--- ebpf/cgen_test.go
+++ ebpf/cgen_test.go
@@ -1,49 +1,57 @@
 package ebpf
 
 import (
-	"log"
-	"os"
-	"strings"
 	"testing"
-	"text/template"
 
 	"github.com/mehrdadrad/tcpdog/config"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestX(t *testing.T) {
-	var funcMap = template.FuncMap{
-		"isBPF": strings.HasPrefix,
-	}
-
+func TestGetBPFCode(t *testing.T) {
 	cfgTracepoint := config.Tracepoint{
-		Name:            "tcp:tcp_retransmit_skb",
-		Fields:          "custom_fields1",
-		TCPState:        "close",
-		PollingInterval: 100,
-		Inet:            []int{4, 6},
-		Geo:             "enable",
+		Name:     "sock:inet_sock_set_state",
+		Fields:   "custom_fields1",
+		TCPState: "TCP_CLOSE",
+		Inet:     []int{4, 6},
 	}
 
 	cfgFileds := map[string][]config.Field{
 		"custom_fields1": {
-			{Name: "SRTT", Func: "/1000", Filter: ">1000"},
+			{Name: "SRTT", Func: "/1000", Filter: "SRTT>1000"},
 			{Name: "TotalRetrans", Func: "", Filter: ""},
+			{Name: "SAddr"},
+			{Name: "DAddr"},
 		},
 	}
 
-	fields4 := getReqFieldsV4(cfgFileds[cfgTracepoint.Fields])
-	log.Printf("%#v", fields4)
+	source, err := GetBPFCode(&config.Config{
+		Tracepoints: []config.Tracepoint{cfgTracepoint},
+		Fields:      cfgFileds,
+	})
 
-	tt := TracepointTemplate{
-		Fields4:    fields4,
-		Tracepoint: cfgTracepoint.Name,
-		TCPState:   cfgTracepoint.TCPState,
-		Suffix:     1,
-	}
-	tt.Init()
-	tmpl, err := template.New("source").Funcs(funcMap).Parse(source)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl.Execute(os.Stdout, tt)
+	assert.NoError(t, err)
+
+	assert.Contains(t, source, "struct tcp_sock *tcpi = (struct tcp_sock *)sk;")
+	assert.Contains(t, source, "u32 srtt_us;")
+	assert.Contains(t, source, "u32 total_retrans;")
+	assert.Contains(t, source, "args->newstate != TCP_CLOSE")
+	assert.Contains(t, source, "args->protocol != IPPROTO_TCP")
+
+	// v4
+	assert.Contains(t, source, "data4.srtt_us>1000")
+	assert.Contains(t, source, "ipv4_events0.perf_submit(args, &data4, sizeof(data4)")
+	assert.Contains(t, source, "data4.srtt_us = tcpi->srtt_us/1000;")
+	assert.Contains(t, source, "data4.total_retrans = tcpi->total_retrans;")
+	assert.Contains(t, source, "BPF_PERF_OUTPUT(ipv4_events0);")
+	assert.Contains(t, source, "u32 skc_rcv_saddr;")
+	assert.Contains(t, source, "u32 skc_daddr;")
+
+	// v6
+	assert.Contains(t, source, "data6.srtt_us>1000")
+	assert.Contains(t, source, "ipv6_events0.perf_submit(args, &data6, sizeof(data6)")
+	assert.Contains(t, source, "data6.srtt_us = tcpi->srtt_us/1000;")
+	assert.Contains(t, source, "data6.total_retrans = tcpi->total_retrans;")
+	assert.Contains(t, source, "BPF_PERF_OUTPUT(ipv6_events0);")
+	assert.Contains(t, source, "unsigned __int128 skc_v6_rcv_saddr;")
+	assert.Contains(t, source, "unsigned __int128 skc_v6_daddr;")
 }
