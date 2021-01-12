4854eca9e3ea466e5d4fe4341a4191ee8d41c790
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2021-01-11 21:11:55 -0800

    add united states abbr map

--- server/geo/ip2loc/ip2loc.go
+++ server/geo/ip2loc/ip2loc.go
@@ -9,11 +9,15 @@ import (
 var db *ip2location.DB
 
 // Geo represents ip2location
-type Geo struct{}
+type Geo struct {
+	usCodes map[string]string
+}
 
 // New constructs Geo
 func New() *Geo {
-	return &Geo{}
+	return &Geo{
+		usCodes: usCodes(),
+	}
 }
 
 // Init initializes ip2location database
@@ -34,9 +38,66 @@ func (i *Geo) Get(ip string) map[string]string {
 		return nil
 	}
 	return map[string]string{
-		"counter_code": info.Country_short,
-		"country":      info.Country_long,
-		"region":       info.Region,
-		"city":         info.City,
+		"CCode":   info.Country_short,
+		"Country": info.Country_long,
+		"Region":  info.Region,
+		"City":    info.City,
+		"USCode":  i.usCodes[info.Region],
+	}
+}
+
+func usCodes() map[string]string {
+	return map[string]string{
+		"Alabama":              "AL",
+		"Alaska":               "AK",
+		"Arizona":              "AZ",
+		"Arkansas":             "AR",
+		"California":           "CA",
+		"Colorado":             "CO",
+		"Connecticut":          "CT",
+		"Delaware":             "DE",
+		"District Of Columbia": "DC",
+		"Florida":              "FL",
+		"Georgia":              "GA",
+		"Hawaii":               "HI",
+		"Idaho":                "ID",
+		"Illinois":             "IL",
+		"Indiana":              "IN",
+		"Iowa":                 "IA",
+		"Kansas":               "KS",
+		"Kentucky":             "KY",
+		"Louisiana":            "LA",
+		"Maine":                "ME",
+		"Maryland":             "MD",
+		"Massachusetts":        "MA",
+		"Michigan":             "MI",
+		"Minnesota":            "MN",
+		"Mississippi":          "MS",
+		"Missouri":             "MO",
+		"Montana":              "MT",
+		"Nebraska":             "NE",
+		"Nevada":               "NV",
+		"New Hampshire":        "NH",
+		"New Jersey":           "NJ",
+		"New Mexico":           "NM",
+		"New York":             "NY",
+		"North Carolina":       "NC",
+		"North Dakota":         "ND",
+		"Ohio":                 "OH",
+		"Oklahoma":             "OK",
+		"Oregon":               "OR",
+		"Pennsylvania":         "PA",
+		"Rhode Island":         "RI",
+		"South Carolina":       "SC",
+		"South Dakota":         "SD",
+		"Tennessee":            "TN",
+		"Texas":                "TX",
+		"Utah":                 "UT",
+		"Vermont":              "VT",
+		"Virginia":             "VA",
+		"Washington":           "WA",
+		"West Virginia":        "WV",
+		"Wisconsin":            "WI",
+		"Wyoming":              "WY",
 	}
 }
