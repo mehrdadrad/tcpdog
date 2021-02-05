2f7f644ae5cbca6875953766d9f6a3640c3f884f
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2021-02-04 19:37:32 -0800

    add more tests

--- ingress/kafka/kafka_test.go
+++ ingress/kafka/kafka_test.go
@@ -1,13 +1,16 @@
 package kafka
 
 import (
+	"context"
 	"testing"
 
+	"github.com/Shopify/sarama"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/structpb"
 
+	"github.com/mehrdadrad/tcpdog/config"
 	pb "github.com/mehrdadrad/tcpdog/proto"
 )
 
@@ -59,4 +62,61 @@ func TestGetUnmarshalSPB(t *testing.T) {
 	// cover nil
 	f = getUnmarshal("unknown")
 	assert.Nil(t, f)
+}
+
+func TestStart(t *testing.T) {
+	broker := sarama.NewMockBroker(t, 0)
+	defer broker.Close()
+
+	// resp := new(sarama.MetadataResponse)
+	// broker.Returns(resp)
+	mockFetchResponse := sarama.NewMockFetchResponse(t, 1)
+	for i := 0; i < 10; i++ {
+		mockFetchResponse.SetMessage("tcpdog", 0, int64(i+1234), sarama.StringEncoder(`{"v1":"k1"}`))
+	}
+
+	broker.SetHandlerByMap(map[string]sarama.MockResponse{
+		"MetadataRequest": sarama.NewMockMetadataResponse(t).
+			SetBroker(broker.Addr(), broker.BrokerID()).
+			SetLeader("tcpdog", 0, broker.BrokerID()),
+		"OffsetRequest": sarama.NewMockOffsetResponse(t).
+			SetOffset("tcpdog", 0, sarama.OffsetOldest, 0).
+			SetOffset("tcpdog", 0, sarama.OffsetNewest, 2345),
+		"FetchRequest": mockFetchResponse,
+	})
+
+	cfg := config.ServerConfig{
+		Ingress: map[string]config.Ingress{
+			"foo": {
+				Config: map[string]interface{}{
+					"brokers": []string{broker.Addr()},
+					"topic":   "tcpdog",
+				},
+			},
+		},
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = cfg.WithContext(ctx)
+	ch := make(chan interface{}, 1)
+	err := Start(ctx, "foo", "json", ch)
+	assert.NoError(t, err)
+}
+
+func TestSaramaConfig(t *testing.T) {
+	conf := &Config{
+		Brokers:      []string{"localhost:9092"},
+		Topic:        "tcpdog",
+		RetryBackoff: 2,
+		Workers:      2,
+		Version:      "0.10.2.1",
+		TLSConfig: config.TLSConfig{
+			Enable:   true,
+			Insecure: true,
+		},
+	}
+
+	_, err := saramaConfig(conf)
+	assert.NoError(t, err)
 }
