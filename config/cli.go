291f8a7a8e525b8393f2a67ee486642718900fb1
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2021-02-11 19:38:31 -0800

    update usage

--- config/cli.go
+++ config/cli.go
@@ -15,10 +15,10 @@ import (
 )
 
 var flags = []cli.Flag{
-	&cli.BoolFlag{Name: "ipv4", Aliases: []string{"4"}, Usage: "enable IPv4 address", DefaultText: "true if IPv6 is false"},
+	&cli.BoolFlag{Name: "ipv4", Aliases: []string{"4"}, Usage: "enable IPv4 address", DefaultText: "true if ipv6 is false"},
 	&cli.BoolFlag{Name: "ipv6", Aliases: []string{"6"}, Usage: "enable IPv6 address"},
 	&cli.StringFlag{Name: "tracepoint", Aliases: []string{"tp"}, Value: "sock:inet_sock_set_state", Usage: "tracepoint name"},
-	&cli.StringFlag{Name: "fields", Aliases: []string{"f"}, Value: "srtt,saddr,daddr,dport", Usage: "fields"},
+	&cli.StringFlag{Name: "fields", Aliases: []string{"f"}, Value: "rtt,totalretrans,saddr,daddr,dport", Usage: "tcp fields"},
 	&cli.StringFlag{Name: "state", Aliases: []string{"s"}, Value: "TCP_CLOSE", Usage: "tcp state"},
 	&cli.StringFlag{Name: "config", Aliases: []string{"c"}, Value: "", Usage: "path to a file in yaml format to read configuration"},
 	&cli.IntFlag{Name: "sample", Aliases: []string{"a"}, Value: 0, Usage: "sample rate"},
